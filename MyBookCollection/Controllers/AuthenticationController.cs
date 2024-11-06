4003ea94ad8d62ac0a483e1bd08f306321282117
souravkunduss1@outlook.com <souravkunduss1@outlook.com>
2024-11-07 00:48:04 +0600

    AuthenticationLayoutPage Added# Login View Page added# Custom theme added for fronend#

---
+++ MyBookCollection/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBookCollection.Models.Data;
+using MyBookCollection.Services;
+
+namespace MyBookCollection.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        //private IUserService _userService;
+        private SignInManager<AppUser> _signInManager;
+        private UserManager<AppUser> _userManager;
+
+        public AuthenticationController( SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            //_userService = userService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        //Login users
+        public async Task<IActionResult> Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ MyBookCollection/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using MyBookCollection.Helpers.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBookCollection.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [CustomEmailValidator(ErrorMessage = "Email is not valid(custom)")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password should contain atleast 6 characters.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
